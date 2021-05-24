a217177eb47d8679c1ca55c466c1f52c0e490c86
willian oliveira <willian.lemes@iatec.com>
2021-05-24 12:11:54 -0300

    Add Dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:github_search_arq/app_module.dart';
+import 'package:github_search_arq/modules/app_widget.dart';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Container(),
-    );
-  }
+  runApp(ModularApp(module: AppModule(), child: AppWidget()));
 }
---
+++ test/app_module_test.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter_modular_test/flutter_modular_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:github_search_arq/app_module.dart';
+import 'package:github_search_arq/modules/search/domain/entities/result_search.dart';
+import 'package:github_search_arq/modules/search/domain/usecases/search_by_text.dart';
+import 'package:mockito/mockito.dart';
+
+import 'modules/external/utils/github_response.dart';
+
+class DioMock extends Mock implements Dio {}
+
+main() {
+  final dio = DioMock();
+
+  initModule(AppModule(), replaceBinds: [
+    Bind<Dio>((i) => dio),
+  ]);
+
+  test("Deve recuperar o usecase sem erro", () {
+    final usecase = Modular.get<SearchByText>();
+
+    expect(usecase, isA<SearchByTextImpl>());
+  });
+
+  test("Deve trazer uma lista de ResultSeachr", () async {
+    when(dio.get(any)).thenAnswer((_) async => Response(
+        data: jsonDecode(githubResult), statusCode: 200, requestOptions: null));
+
+    final usecase = Modular.get<SearchByText>();
+    final result = await usecase("Will");
+
+    expect(result | null, isA<List<ResultSearch>>());
+  });
+}
